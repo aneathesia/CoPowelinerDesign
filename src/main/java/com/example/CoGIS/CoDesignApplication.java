cffce16c605e0bd1ec3628f77c146f1abcac8176
aneathesia <2394251482@qq.com>
2022-01-04 15:36:57 +0800

    initial the CoGIS project

---
+++ src/main/java/com/example/CoGIS/CoDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.CoGIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoDesignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoDesignApplication.class, args);
+    }
+
+}
